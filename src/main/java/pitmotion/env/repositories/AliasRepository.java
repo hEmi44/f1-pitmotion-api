20aba112c948506dbed1194fb7b2c340067a670f
hEmi44 <emilien.plaitin07@hotmail.com>
2025-08-27 23:12:10 +0200

    🚑️ Fix

--- src/main/java/pitmotion/env/repositories/AliasRepository.java
+++ src/main/java/pitmotion/env/repositories/AliasRepository.java
@@ -1,10 +1,12 @@
 package pitmotion.env.repositories;
 
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import pitmotion.env.entities.Alias;
 import pitmotion.env.enums.EntityType;
 
-public interface AliasRepository extends CrudRepository<Alias,Long> {
+public interface AliasRepository extends JpaRepository<Alias,Long> {
   Optional<Alias> findByEntityTypeAndAlias(EntityType entityType, String alias);
 }
